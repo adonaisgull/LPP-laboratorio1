d818a63df8753052dd1223da58fb8d1eec617dd1
SUAREZ GONZALEZ <alu4308@cc150.(none)>
2012-10-04 15:22:13 +0100

    Añadimos saludo personalizado al hola_mundo.rb

--- hola_mundo.rb
+++ hola_mundo.rb
@@ -1,1 +1,9 @@
-puts "Hola mundo"
+#
+# Laboratorio 1 LPP
+#
+
+# Obtenemos el nombre del usuario por la shell
+user = ARGV.shift || "nobody"
+
+# Mostramos mensaje con el nombre del usuario interpolado
+puts "Hello world, #{user.capitalize}!"
